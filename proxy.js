cc336836508449bd3faf7c44a809b62b45b9e2d1
Daniel Simmons <dan@dans.im>
2011-05-13 21:01:40 +0100

    No longer using proxy-utils module.

--- proxy.js
+++ proxy.js
@@ -1,5 +1,5 @@
-var http_proxy = require("./lib/proxy");
-var utils = require("./lib/proxy-utils");
+var util = require('util');
+var http_proxy = require('./lib/proxy');
 
 // Configuration - TODO: move to config file
 var listen_port = 80;
@@ -8,4 +8,4 @@ var proxy = http_proxy.createServer({
 	"targetHost": "213.129.83.20",
 	"targetPort": "80",
 }).listen(listen_port);
-utils.log("master: Listening on port " + listen_port);
+util.log("master: Listening on port " + listen_port);
