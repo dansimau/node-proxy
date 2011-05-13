aa581a6824769b9883c89853dd39ad0b7ae97aaa
Daniel Simmons <dan@dans.im>
2011-05-13 19:54:28 +0100

    First part of major refactor.

--- proxy.js
+++ proxy.js
@@ -4,5 +4,8 @@ var utils = require("./lib/proxy-utils");
 // Configuration - TODO: move to config file
 var listen_port = 80;
 
-var proxy = http_proxy.createServer().listen(listen_port);
+var proxy = http_proxy.createServer({
+	"targetHost": "213.129.83.20",
+	"targetPort": "80",
+}).listen(listen_port);
 utils.log("master: Listening on port " + listen_port);
