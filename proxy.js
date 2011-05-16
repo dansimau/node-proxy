cd1ad3e2cc0a7bed4f37557d957dc30f54a818e3
Daniel Simmons <dan@dans.im>
2011-05-16 19:03:06 +0100

    Cache is now configurable from top-level.

--- proxy.js
+++ proxy.js
@@ -7,5 +7,8 @@ var listen_port = 80;
 var proxy = http_proxy.createServer({
 	"targetHost": "213.129.83.20",
 	"targetPort": "80",
+	cache: {
+		maxMem: 16
+	}
 }).listen(listen_port);
 util.log("master: Listening on port " + listen_port);
