fcc6c697b8c200629e57df84dfefe905b4b13056
Daniel Simmons <dan@dans.im>
2011-05-17 21:19:55 +0100

    Move configuration to a separate file.

--- proxy.js
+++ proxy.js
@@ -1,14 +1,24 @@
 var util = require('util');
 var http_proxy = require('./lib/proxy');
+var fs = require('fs');
 
-// Configuration - TODO: move to config file
-var listen_port = 80;
+var configFile = __dirname + '/proxy.conf';
+var conf = {};
 
-var proxy = http_proxy.createServer({
-	"targetHost": "213.129.83.20",
-	"targetPort": "80",
-	cache: {
-		maxMem: 16
-	}
-}).listen(listen_port);
-util.log("master: Listening on port " + listen_port);
+try {
+	conf = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+}
+catch (err) {
+	util.log('Unable to read configuration from file (' + configFile + ').');
+	util.log('Check that the file exists and syntax is correct.');
+	process.exit(1);
+}
+
+for (var i = 0, l = conf.length; i < l; i++) {
+	var listen_port = conf[i].port;
+	var proxy = http_proxy.createServer(
+		conf[i].proxy
+	).listen(conf[i].port, function() {
+		util.log("Listening on port " + listen_port);
+	});
+}
